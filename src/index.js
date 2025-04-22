9c0888196de4aef9b89ae069797fdac02b161c17
ozgur <ozgur@ozgurs-Mac-mini.local>
2025-04-22 16:25:46 +0300

    fix: consistent bootstrap casing and missing imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export { CsvReader } from './core/data/CsvReader.js';
 export { BinAnalyzer } from './core/analysis/bin/BinAnalyzer.js';
-export { Bootstrapper } from './core/analysis/Bootstrap/Bootstrapper.js';
+export { Bootstrapper } from './core/analysis/bootstrap/Bootstrapper.js';
